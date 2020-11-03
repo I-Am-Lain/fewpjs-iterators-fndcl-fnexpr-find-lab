e6047495de534e7e3fb382a9471fa4cbf77dcaba
I-Am-Lain <vetter.william@gmail.com>
2020-11-03 00:06:13 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let myAnswer = record.find(function(e) {return e.result === 'W'})
+  if (myAnswer){
+    return myAnswer['year']
+  } else myAnswer
+}
